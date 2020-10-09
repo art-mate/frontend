2aa039be7531fd3dc6f3ffec3de4e19f2efeb257
JunSeong Kim <sd99004@korea.ac.kr>
2020-10-09 17:31:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  background-color: yellow;`
 
 function App() {
-  return <div className="App">ARTMATE</div>;
+  return <Main>Artmate</Main>;
 }
 
 export default App;
