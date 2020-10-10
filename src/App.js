9b0178c5e6a1eec713c45dbf17699c87913d0a47
ginger-kang <kdhoon07@gmail.com>
2020-10-11 02:01:59 +0900

    :sparkles: Add global-styles and fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import { GlobalStyle } from './global-styles';
+
+const Main = styled.div`
+  background-color: orange;
+`;
 
 function App() {
-  return <div className="App">ARTMATE</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Main>안녕하세요</Main>
+    </>
+  );
 }
 
 export default App;
