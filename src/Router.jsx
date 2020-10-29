2d88985a6435f7daae136f729049dacd067aefa5
ginger-kang <kdhoon07@gmail.com>
2020-10-30 02:52:27 +0900

    :framed_picture: paints menu page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './routes/Home';
+import Menu from './routes/Menu';
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
         </Switch>
       </BrowserRouter>
     </>
